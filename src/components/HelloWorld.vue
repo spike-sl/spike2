f7c74c0cc4d36062fd59975846fb8dd24f8a2750
spike <160619301@qq.com>
2026-09-20 13:36:02 +0800

    vue基础练习

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- v-bind可省略，前面只加个： -->
+  <div v-bind:class="class">绿龙</div>
+  <div v-bind:id="id">肥美</div>
+
+  <button :disabled="case">确定</button>
+  
+  <div v-bind="object">猎鹰</div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return{
+        class:"spirit",
+        id:"faze",
+        // case:false
+        case:true,
+
+        object:{
+          id:"niko",
+          class:"monesy"
+        }
+            }
+    }
+  }
+</script>
