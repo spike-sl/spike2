46751865b09cafcd37af1b6f4b7c96c7aed73dd7
spike <160619301@qq.com>
2026-09-20 20:41:19 +0800

    vue基础学习

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import IfDemo from './components/IfDemo.vue'
 </script>
 
 <template>
   <HelloWorld/>
+  <IfDemo/>
 </template>
 
 
---
+++ src/components/IfDemo.vue
@@ -0,0 +1,25 @@
+<template>
+    <h2>条件渲染</h2>
+    <div v-if="flag">你能看见我吗</div>
+    <div v-else>你会看到这条</div>
+
+    <div v-if="type === 'A'">a</div>
+    <div v-else-if="type === 'B'">b</div>
+    <div v-else-if="type === 'C'">c</div>
+    <div v-else>Not a/b/c</div>
+
+    <div v-show="flag">这条也能看见</div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                flag:true,
+                // flag:false,
+                type:"D"
+            }
+        }
+    }
+
+</script>
