acdb9a947b40a23bcadcd1a5cc88cc6c3eb782f6
spike <160619301@qq.com>
2026-09-20 23:19:02 +0800

    key属性学习

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import HelloWorld from './components/HelloWorld.vue'
 import IfDemo from './components/IfDemo.vue'
 import ListDemo from './components/ListDemo.vue'
+import KeyDemo from './components/KeyDemo.vue'
 </script>
 
 <template>
   <HelloWorld/>
   <IfDemo/>
   <ListDemo/>
+  <KeyDemo/>
 </template>
 
 
---
+++ src/components/KeyDemo.vue
@@ -0,0 +1,39 @@
+<template>
+    <h2>key属性添加</h2>
+
+    <!-- 尽量使用id作为key的数据而不是index，这是为了确保key的唯一不变性 -->
+    <div>
+        <p v-for="(item,index) of names" :key="index">{{ item }}</p>
+    </div>
+
+    <div v-for="item of result" :key="item.id">
+        <p>{{ item.title }}</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        data() {
+            return {
+                names:["spirit","faze","tyloo"],
+
+                result:[
+                {  
+                    "id":1,
+                    "title":"spitit",
+                    "NOone":"donk",
+                },
+                {  
+                    "id":10,
+                    "title":"navi",
+                    "NOone":"bit",
+                },{  
+                    "id":20,
+                    "title":"faze",
+                    "NOone":"fozen",
+                },
+            ]
+            }
+        }
+    }
+</script>
